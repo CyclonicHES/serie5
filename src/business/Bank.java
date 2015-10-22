2bf67f933e552e76db780baff4d0db7eeda81ee6
Luc Chèvre <luc.chevre@he-arc.ch>
2015-10-22 08:58:17 +0200

    ajout de bank

---
+++ src/business/Bank.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package business;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author luc.chevre
+ */
+public class Bank {
+
+    private int number;
+    private String name;
+    private ArrayList<Customer> customers;
+    private ArrayList<Account> accounts;
+    
+    
+
+    
+}
